b6703569348de7f2e093aab80c18c193a64d90c1
shivanipatil2909 <144975418+shivanipatil2909@users.noreply.github.com>
2025-04-16 22:13:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -598,4 +598,4 @@ if __name__ == "__main__":
     # Optionally notify admin
     threading.Thread(target=send_test_message).start()
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
